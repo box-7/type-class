f783c119d6b0f3d7f8aee7c24c6fbbee6469017d
box-7 <hayainada1981@yahoo.co.jp>
2024-12-03 21:33:08 +0900

    いったんコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,55 @@
 // import { Button, Box, Table } from "@chakra-ui/react";
-import { Table } from "@chakra-ui/react";
+
 import { useEffect } from "react";
 import { GetAllTodos } from "./lib/todo";
-// import { RiArrowRightLine, RiMailLine } from "react-icons/ri"
-// import { chakra } from "@chakra-ui/react"
-// import { VStack } from '@chakra-ui/react';
 
 import { useState } from "react";
 import { Todo } from "./domain/todo";
+import { Table } from "@chakra-ui/react";
 
 export default function App() {
         const [todos, setTodos] = useState<Todo[]>([]);
+        const [isLoading, setIsLoading] = useState<boolean>(true);
 
         useEffect(() => {
                 const getAllTodosMethod = async () => {
                         const todoData = await GetAllTodos();
                         // console.log(todoData);
                         setTodos(todoData);
+                        setIsLoading(false);
                 }
 
                 getAllTodosMethod();
         }
         , []);
 
+        if (isLoading) {
+                return <h1>Loading...</h1>
+        }
+
         return (
                 <>
-                        <h1>Hello World</h1>
-                        {/* <ul> */}
-                                {/* {todos.map((todo) => (
-                                        <li key={todo.id}><Box>{todo.title}</Box></li>
-                                ))} */}
+                        {/* <h1>Hello World</h1> */}
 
-                                {/* <Table.Root size="sm">
+                        <Table.Root size="sm">
                                 <Table.Header>
                                         <Table.Row>
-                                        <Table.ColumnHeader>Title</Table.ColumnHeader>
-                                        <Table.ColumnHeader>done</Table.ColumnHeader>
-                                        <Table.ColumnHeader textAlign="end">CreatedAt</Table.ColumnHeader>
+                                                <Table.ColumnHeader>Title</Table.ColumnHeader>
+                                                <Table.ColumnHeader>done</Table.ColumnHeader>
+                                                <Table.ColumnHeader textAlign="end">CreatedAt</Table.ColumnHeader>
                                         </Table.Row>
                                 </Table.Header>
                                 <Table.Body>
                                         {todos.map((item) => (
                                                 <Table.Row key={item.id}>
-                                                <Table.Cell>{item.title}</Table.Cell>
-                                                <Table.Cell>{item.done ? "TRUE" : "FALSE"}</Table.Cell>
-                                                <Table.Cell textAlign="end">{item.created_at}</Table.Cell>
-                                        </Table.Row>
+                                                        <Table.Cell>{item.title}</Table.Cell>
+                                                        <Table.Cell>{item.done ? "TRUE" : "FALSE"}</Table.Cell>
+                                                        <Table.Cell textAlign="end">{item.created_at}</Table.Cell>
+                                                </Table.Row>
                                         ))}
                                 </Table.Body>
-                                </Table.Root>
-                        </ul> */}
-                        {/* <VStack>
-                                <Button bg="red.300/40" >Button</Button>
-                                <Button  bgColor="red.200" >Button</Button>
+                        </Table.Root>
 
-                                <Button colorPalette="teal" variant="solid" >
-                                        <RiMailLine /> Email
-                                </Button>
-
-                                <Button colorPalette="red" variant="outline">
-                                        Call us <RiArrowRightLine />
-                                </Button>
-
-                                <chakra.button
-                                        // borderWidth="1px"
-                                        // colorPalette="red"
-                                        // borderColor="colorPalette.500"
-                                        bgColor="red.200"
-                                        _hover={{
-                                                borderColor: "colorPalette.100",
-                                                backgroundColor: "red.800",
-                                        }}
-                                >
-                                        Click me
-                                </chakra.button>
-                        </VStack> */}
                 </>
         )
 }
